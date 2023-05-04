6d927b812880951599be2b1c09651c6c3a7900cd
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-05-05 02:15:15 +0500

    add let_const,js

---
+++ ES6_features/let_const.js
@@ -0,0 +1,23 @@
+// Let & Const Keywords 
+
+function sayHello() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i)
+    }
+
+    console.log(i) // still accessible.
+}
+
+sayHello()
+
+// Solving problem with 'let' keyword
+
+function sayHello() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i)
+    }
+
+    // console.log(i) // not accessible.
+}
+
+sayHello()
