03243e88f266712f98dd6d15405c993560a2ff94
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-05-12 02:08:57 +0500

    modify index.css

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = 'Second Component'
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>First React Component</h1>
         <h2>{ title }</h2>
---
+++ react_app/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Ammar's Website</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
