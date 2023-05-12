18a6ab4b19107162ff4ef29753b270164eb348a4
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-05-12 12:18:25 +0500

    add increment decrement functionality

--- react_app/src/Home.js
+++ react_app/src/Home.js
@@ -18,13 +18,26 @@ const Home = () => {
         setAge(30);    
     }
 
+    const [value, setValue] = useState(0);
+
+    const handleIncrement = () => {
+        setValue(value + 1);
+    }
+
+    const handleDecrement = () => {
+        setValue(value - 1);
+    }
+
     return (
         <div className="home">
             <h1>Welcome to My Homepage</h1>
             <button onClick={handleClick}>Click Me</button>
             <button onClick={(e) => handleClickWithArgument('Ammar', e)}>Click Me Again</button>
             <p>My Name is {name} and I am {age} years old</p>
-            <button onClick={handleChange}>Change</button>
+            <button onClick={handleChange}>Change</button> <br />
+            <button onClick={handleIncrement}>+</button>
+            <span>{value}</span>
+            <button onClick={handleDecrement}>-</button>
         </div>
     );
 }
