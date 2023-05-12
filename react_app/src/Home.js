af0756e9092bc6def8094345bc25f1b023b02906
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-05-12 12:04:00 +0500

    use useState as hook function to make variables reactive

--- react_app/src/Home.js
+++ react_app/src/Home.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const Home = () => {
 
     const handleClick = (e) => {
@@ -8,13 +10,21 @@ const Home = () => {
         console.log(`Hello ${name}, handleClickWithArgument Fired!!`);
     }
 
-    const name = "Ammar";
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(20);
+
+    const handleChange = (e) => {
+        setName("Luigi");
+        setAge(30);    
+    }
 
     return (
         <div className="home">
             <h1>Welcome to My Homepage</h1>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickWithArgument(name, e)}>Click Me Again</button>
+            <button onClick={(e) => handleClickWithArgument('Ammar', e)}>Click Me Again</button>
+            <p>My Name is {name} and I am {age} years old</p>
+            <button onClick={handleChange}>Change</button>
         </div>
     );
 }
