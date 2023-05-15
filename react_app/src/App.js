e9f4b96ed07ebb6c15890a2b26c621870a15a61c
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-05-15 22:39:53 +0500

    Demo Router, Route, Switch, and Link

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,17 +1,28 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Welcome from './Welcome';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   const title = 'Second Component'
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <h1>First React Component</h1>
-        <h2>{ title }</h2>
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path='/'>
+              <h1>First React Component</h1>
+              <h2>{ title }</h2>
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <Welcome title={ "" } name={ "Ahmed" }/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- react_app/src/Navbar.js
+++ react_app/src/Navbar.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Ammar's Website</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/" style={{
+                <Link to="/">Home</Link>
+                <Link to="/about" style={{
                     color: 'white',
                     backgroundColor:'#f1356d',
                     borderRadius: '8px'
-                }}>About</a>
+                }}>About</Link>
             </div>
         </nav>
     );
