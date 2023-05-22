3f0eb180337cd99a25c46e04999f2da67c6f6cc8
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-05-22 23:53:01 +0500

    controlled input forms

--- react_app/src/SignUp.js
+++ react_app/src/SignUp.js
@@ -7,12 +7,20 @@ class SignUp extends React.Component {
         this.state = {
             fullname: 'Ammar',
             city: 'Shikarpur',
-            email: 'ammar@gmail.com'
+            email: 'ammar@gmail.com',
+            isMale: true,
+            language: ['English', 'Norwegian']
         }
     }
 
     handleChange(event) {
-        const { name, type, value } = event.target;
+        let { name, type, value, checked, selectedOptions } = event.target;
+        
+        if (name === 'language')
+            value = Array.from(selectedOptions, option => option.value);
+        else if (type === "checkbox")
+            value = checked;
+        
         this.setState({...this.state, [name]: value});
     }
 
@@ -26,6 +34,17 @@ class SignUp extends React.Component {
                 <input type="text" name="city" value={this.state.city} onChange={this.handleChange}/>
                 <label for="email">Email:</label>
                 <input type="text" name="email" value={this.state.email} onChange={this.handleChange}/>
+
+                <label for="isMale">Are you a male? </label>
+                <input type="checkbox" name="isMale" checked={this.state.isMale} onChange={this.handleChange}/>
+
+                <label for="language"></label>
+                <select name="language" value={this.state.language} onChange={this.handleChange} multiple={true}>
+                    <option value="English">English</option>
+                    <option value="Sindhi">Sindhi</option>
+                    <option value="Norwegian">Norwegian</option>
+                    <option value="Urdu">Urdu</option>
+                </select>
             </form>
         </>
     }
