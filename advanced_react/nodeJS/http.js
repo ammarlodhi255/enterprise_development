ae333d081f1afd6412de19446607d36606f5fa48
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-06-21 22:51:30 +0500

    sending a html file as a response

--- advanced_react/nodeJS/http.js
+++ advanced_react/nodeJS/http.js
@@ -1,21 +1,31 @@
 const http = require("node:http");
 
+// SEND A PLAIN TEXT RESPONSE
 // const server = http.createServer((req, res) => {
 //     res.writeHead(200, { "Content-Type": "text/plain"});
 //     res.end("Here is the response!");
 // });
 
+// SEND A JSON RESPONSE
+// const server = http.createServer((req, res) => {
+//     const person = {
+//         name: "Ammar",
+//         age: 23
+//     }
+
+//     res.writeHead(200, { "Content-Type": "application/json"});
+//     res.end(JSON.stringify(person));
+// })
+
+// SEND A HTML RESPONSE
+const path = require("node:path");
+const fs = require("node:fs")
 
 const server = http.createServer((req, res) => {
-    const person = {
-        name: "Ammar",
-        age: 23
-    }
-
-    res.writeHead(200, { "Content-Type": "application/json"});
-    res.end(JSON.stringify(person));
-})
-
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const html = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
+    res.end(html);
+});
 
 server.listen(3000, () => {
     console.log("Server listening on port 3000");
