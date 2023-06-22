28becab078062e05f58b5aab97a81f9ab3e9c44a
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-06-23 00:51:48 +0500

    add mongoose

--- Node.js/app.js
+++ Node.js/app.js
@@ -2,6 +2,12 @@ const path = require("node:path")
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/StudentDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+});
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
